7a4858fdee56c77c5f3794298e83d49414f4362b
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-05-04 23:57:34 +0545

    Show error if project already initialized

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,10 +1,18 @@
-use crate::structures::error::Error;
+use crate::structures::error::{Error, ErrorKind};
 use crate::{commands::init, modules::project::*};
 
 pub fn init(project_alias: &String) -> Result<(), Error> {
     create_yo_dir()?;
 
     // check if project is already initialized
+    if !is_project_initialized(project_alias)? {
+        create_project_dir(project_alias)?
+    } else {
+        return Err(Error::new(format!(
+            "Project already initialized with alias `{project_alias}`"
+        ))
+        .kind(ErrorKind::Project));
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #![allow(unused)]
+mod commands;
+mod modules;
 mod structures;
 mod utils;
 
 use clap::Parser;
+use commands::init;
 use structures::args::{Command, YoArgs};
+use utils::print::{fprint, MessageType};
 
 fn main() {
     let args = YoArgs::parse();
 
     match args.command {
-        Command::Init(x) => println!("{}", x.alias),
+        Command::Init(x) => match init::init(&(x.alias)) {
+            Ok(_) => fprint("Initialized".to_string(), MessageType::Success),
+            Err(e) => fprint(e.to_string(), MessageType::Error),
+        },
         Command::Workon(x) => println!("{}", x.alias),
     }
 }
