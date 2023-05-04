ebd61d8af23081f336775d97145c1248e3848dab
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-05-04 21:04:35 +0545

    Add builder pattern to error struct for more pragmatic error building

--- src/structures/error.rs
+++ src/structures/error.rs
@@ -13,10 +13,10 @@ pub enum ErrorKind {
 
 #[derive(Debug, Error)]
 pub struct Error {
-    message: String,
+    pub message: String,
     pub code: i32,
-    kind: ErrorKind,
-    origin: String,
+    pub kind: ErrorKind,
+    pub origin: String,
 }
 
 impl std::fmt::Display for Error {
@@ -30,40 +30,33 @@ impl std::fmt::Display for Error {
 }
 
 impl Error {
-    pub fn new<T: std::error::Error>(
-        message: String,
-        code: i32,
-        kind: ErrorKind,
-        source: T,
-    ) -> Self {
+    pub fn new<T: std::error::Error>(message: String) -> Error {
+        Error {
+            message,
+            code: exitcode::TEMPFAIL,
+            kind: ErrorKind::Unknown,
+            origin: String::from("None"),
+        }
+    }
+
+    pub fn code(mut self, code: i32) -> Error {
+        self.code = code;
+        self
+    }
+
+    pub fn kind(mut self, kind: ErrorKind) -> Error {
+        self.kind = kind;
+        self
+    }
+
+    pub fn source<T: std::error::Error>(mut self, source: T) -> Error {
         let formatted_source = format!("{:?}", source);
         let source_name = match formatted_source.split_whitespace().nth(0) {
             Some(x) => x.to_string(),
             None => formatted_source,
         };
-        Error {
-            message,
-            code,
-            kind,
-            origin: format!("{source_name} ({})", source.to_string()),
-        }
-    }
 
-    pub fn new_sourceless(message: String, code: i32, kind: ErrorKind) -> Self {
-        Error {
-            message,
-            code,
-            kind,
-            origin: "None".to_string(),
-        }
-    }
-
-    pub fn empty() -> Self {
-        Error {
-            message: "".to_string(),
-            code: -1,
-            kind: ErrorKind::Unknown,
-            origin: "None".to_string(),
-        }
+        self.origin = format!("{source_name} ({})", source.to_string());
+        self
     }
 }
