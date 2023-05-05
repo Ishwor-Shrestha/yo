e73ece74d429c330eed591f41583ec709a3f54d4
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-05-05 11:15:15 +0545

    Add function to read config for given alias

--- src/modules/project.rs
+++ src/modules/project.rs
@@ -23,10 +23,22 @@ pub fn create_project_config(alias: &String) -> Result<(), Error> {
         root: get_current_path()?,
     };
 
+    write_to_file(&get_config_path(alias)?, &config)
+}
+
+pub fn get_config_path(alias: &String) -> Result<String, Error> {
     let home_path = get_home_path()?;
     let config_path = get_file_path(vec![&home_path, ".yo", alias, "config"])?;
 
-    write_to_file(&config_path, &config)
+    Ok(config_path)
+}
+
+pub fn get_config(alias: &String) -> Result<Config, Error> {
+    let content = read_file(&get_config_path(alias)?)?;
+    let config: Config = serde_json::from_str(&content)
+        .map_err(|e| Error::new(format!("Error when deserializing config for `{alias}`")))?;
+
+    Ok(config)
 }
 
 pub fn is_project_initialized(alias: &String) -> Result<bool, Error> {
