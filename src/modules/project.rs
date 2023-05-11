dbf4a72cbdfce6488b9b18a19e8e37e539890179
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-05-12 00:24:48 +0545

    Update get_project_alias function to return correct project alias

--- src/modules/project.rs
+++ src/modules/project.rs
@@ -1,4 +1,4 @@
-use crate::modules::file::*;
+use crate::modules::{file::*, strings::*};
 use crate::structures::{config::Config, error::Error, error::ErrorKind};
 use std::env::current_dir;
 use std::{env, fs, path::Path};
@@ -28,8 +28,44 @@ pub fn if_project_initialized<T>(callback: Callback<T>) -> Result<T, Error> {
 
 // Get project alias
 pub fn get_projec_alias() -> Result<String, Error> {
-    println!("{}", get_current_path()?);
-    let alias = get_current_path()?.replace("/", "_").replace("\\", "_");
+    let home_path = get_home_path()?;
+    let yo_path = get_file_path(vec![&home_path, ".yo"])?;
+
+    let directories = std::fs::read_dir(&yo_path).map_err(|e| {
+        Error::new("Could not read .yo directory".to_string())
+            .kind(ErrorKind::FileSystem)
+            .source(e)
+    })?;
+
+    let current_path_alias = get_current_path()?.replace("/", "_").replace("\\", "_");
+    let mut alias = String::new();
+
+    for directory in directories {
+        let directory = directory.map_err(|e| {
+            Error::new("Could not read .yo directory".to_string())
+                .kind(ErrorKind::FileSystem)
+                .source(e)
+        })?;
+
+        let directory_alias = directory.path().display().to_string().replace(&yo_path, "");
+
+        let directory_alias = crop_letters(&directory_alias, 1)
+            .replace("/", "_")
+            .replace("\\", "_");
+
+        println!("Directory alias : {}", directory_alias);
+
+        if current_path_alias.contains(&directory_alias) {
+            alias = directory_alias;
+            break;
+        }
+    }
+
+    if alias.is_empty() {
+        return Err(
+            Error::new("Project has not been initialized".to_string()).kind(ErrorKind::FileSystem)
+        );
+    }
 
     Ok(alias)
 }
