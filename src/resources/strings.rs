03fb937d46521eacbfcdf16369623e1ce71a9cd2
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-05-26 13:26:31 +0545

    Fix typo

--- src/resources/strings.rs
+++ src/resources/strings.rs
@@ -1,6 +1,6 @@
 pub const S_PROJECT_ALREADY_INITIALIZED: &str = "Project already initialized";
 pub const S_PROJECT_INITIALIZED: &str = "Project initialized";
 pub const S_COULD_NOT_READ_YO_DIR: &str = "Could not read .yo directory";
-pub const S_PROJECT_NOT_INITIALIZESD: &str = "Project has not been initialized";
+pub const S_PROJECT_NOT_INITIALIZED: &str = "Project has not been initialized";
 pub const S_ERROR_DESERIALIZING_CONFIG: &str = "Error when deserializing config";
 pub const S_FAILED_TO_COMMAND: &str = "Failed to run command";
