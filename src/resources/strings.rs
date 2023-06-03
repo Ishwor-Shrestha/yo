7d89e79b30c45892ee882b8b7af0b5d4ad434571
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-06-04 01:06:20 +0545

    Update strings

--- src/resources/strings.rs
+++ src/resources/strings.rs
@@ -5,3 +5,5 @@ pub const S_PROJECT_NOT_INITIALIZED: &str = "Project has not been initialized";
 pub const S_ERROR_DESERIALIZING_CONFIG: &str = "Error when deserializing config";
 pub const S_FAILED_TO_COMMAND: &str = "Failed to run command";
 pub const S_FAILED_TO_OPEN_CONFIG: &str = "Failed to open config";
+pub const S_FAILED_TO_SCAN_PUBSPEC_DIRECTORIES: &str = "Failed to scan pubspec directories";
+pub const S_MAKE_SURE_SCAN_THE_PROJECT_FIRST: &str = "Make sure scan the project first";
