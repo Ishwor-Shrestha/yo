98945262c71aba1b12d89b652cc937cd5b8d5053
Ishwor-Shrestha <ishworisgod@gmail.com>
2023-06-08 02:31:54 +0545

    Add new strings

--- src/resources/strings.rs
+++ src/resources/strings.rs
@@ -10,3 +10,5 @@ pub const S_MAKE_SURE_SCAN_THE_PROJECT_FIRST: &str = "Make sure scan the project
 pub const S_SCANNED_PROJECT_DIRECTORY: &str = "Scanned project directory";
 pub const S_FETCHED_FLUTTER_DEPENDENCIES: &str = "Fetch flutter dependencies";
 pub const S_FAILED_TO_FETCH_FLUTTER_DEPENDENCIES: &str = "Failed to fetch flutter dependencies";
+pub const S_BUILT_FLUTTER_PROJECT: &str = "Build flutter project";
+pub const S_FAILED_TO_BUILD_FLUTTER_PROJECT: &str = "Failed to build flutter project";
